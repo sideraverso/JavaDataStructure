7018d8118f0bd167c32caa6dac13957b521ab441
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-05-10 18:01:54 +0300

    removeIf demo

--- src/eu/javaspecialists/courses/datastructures/ch2_lists/_2_2_ArrayList.java
+++ src/eu/javaspecialists/courses/datastructures/ch2_lists/_2_2_ArrayList.java
@@ -1,5 +1,11 @@
 package eu.javaspecialists.courses.datastructures.ch2_lists;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Adding four seasons
  * indexOf() and contains()
@@ -8,6 +14,32 @@ package eu.javaspecialists.courses.datastructures.ch2_lists;
  */
 public class _2_2_ArrayList {
   public static void main(String... args) {
+    for (int i = 0; i < 10; i++) {
+      test();
+    }
+  }
 
+  private static void test() {
+    List<Integer> numbers;
+    long timeCreate = System.nanoTime();
+    try {
+      numbers = IntStream.range(0, 400_000)
+          .boxed().collect(Collectors.toCollection(ArrayList::new));
+    } finally {
+      timeCreate = System.nanoTime() - timeCreate;
+      System.out.printf("timeCreate = %dms%n", (timeCreate / 1_000_000));
+    }
+    long time = System.nanoTime();
+    try {
+//      for (Iterator<Integer> it = numbers.iterator(); it.hasNext(); ) {
+//        Integer next = it.next();
+//        if (next % 2 == 1) it.remove();
+//      }
+      numbers.removeIf(next -> next % 2 == 1);
+    } finally {
+      time = System.nanoTime() - time;
+      System.out.printf("time = %dms%n", (time / 1_000_000));
+    }
+    System.out.println(numbers.size());
   }
 }
